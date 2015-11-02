bbe20b41c6477bad9364f325b836f923ff590448
Ayon Ghosh <ayon.ghosh@nutanix.com>
2015-11-02 14:33:55 +0530

    Code formatting

--- app/xctrl.js
+++ app/xctrl.js
@@ -6,15 +6,15 @@ module.exports = (function () {
 
   var KEY_CODE = {
     BACKSPACE: 8,
-  ENTER    : 13,
-  SHIFT    : 16,
-  CTRL     : 17,
-  ALT      : 18,
-  LEFT     : 37,
-  UP       : 38,
-  RIGHT    : 39,
-  DOWN     : 40,
-  ESCAPE   : 27
+    ENTER    : 13,
+    SHIFT    : 16,
+    CTRL     : 17,
+    ALT      : 18,
+    LEFT     : 37,
+    UP       : 38,
+    RIGHT    : 39,
+    DOWN     : 40,
+    ESCAPE   : 27
   };
 
   function _initDisplay() {
@@ -52,40 +52,40 @@ module.exports = (function () {
 
   function _keyStroke(key, keyCode, modifierKeyCode) {
     switch (keyCode) {
-    case KEY_CODE.BACKSPACE:
-      _sendNonPrintableKeyStroke('BackSpace');
+      case KEY_CODE.BACKSPACE:
+        _sendNonPrintableKeyStroke('BackSpace');
+        break;
+      case KEY_CODE.ENTER:
+        _sendNonPrintableKeyStroke('Return');
+        break;
+      case KEY_CODE.LEFT:
+        _sendNonPrintableKeyStroke('Left'); // 'leftarrow'
+        break;
+      case KEY_CODE.RIGHT:
+        _sendNonPrintableKeyStroke('Right'); // 'rightarrow'
+        break;
+      case KEY_CODE.UP:
+        _sendNonPrintableKeyStroke('Up');
+        break;
+      case KEY_CODE.DOWN:
+        _sendNonPrintableKeyStroke('Down');
+        break;
+      case KEY_CODE.ESCAPE:
+        _sendNonPrintableKeyStroke('Escape');
+        break;
+      default:
+        exec('xdotool type \'' +
+        (modifier ? _getModifierKeySym(modifierKeyCode) + '+' : '')
+        + ' ' + key + '\'');
       break;
-    case KEY_CODE.ENTER:
-      _sendNonPrintableKeyStroke('Return');
-      break;
-    case KEY_CODE.LEFT:
-      _sendNonPrintableKeyStroke('Left'); // 'leftarrow'
-    break;
-    case KEY_CODE.RIGHT:
-      _sendNonPrintableKeyStroke('Right'); // 'rightarrow'
-    break;
-    case KEY_CODE.UP:
-      _sendNonPrintableKeyStroke('Up');
-    break;
-    case KEY_CODE.DOWN:
-      _sendNonPrintableKeyStroke('Down');
-    break;
-    case KEY_CODE.ESCAPE:
-      _sendNonPrintableKeyStroke('Escape');
-    break;
-    default:
-      exec('xdotool type \'' +
-      (modifier ? _getModifierKeySym(modifierKeyCode) + '+' : '')
-      + ' ' + key + '\'');
-    break;
-  }
+    }
   };
 
   return {
     initDisplay : _initDisplay,
     moveMouse   : _moveMouseRelative,
-  leftClick   : _leftClick,
-  rightClick  : _rightClick,
-  keyStroke   : _keyStroke
+    leftClick   : _leftClick,
+    rightClick  : _rightClick,
+    keyStroke   : _keyStroke
   };
 })();
